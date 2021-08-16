54190a8a351b17d7097714f574f8a09ede2d8273
zhou-ning <1767508581@qq.com>
2021-08-16 21:59:21 +0800

    学习新代码

---
+++ DragAndDraw/app/src/main/java/com/example/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,15 @@
+package com.example.draganddraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+
+}
